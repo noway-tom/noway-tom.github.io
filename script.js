1f48bef42958f32ffd1193761dfa5559c9034713
noway-tom <tomfokuhl@gmail.com>
2026-04-22 20:57:25 +0200

    0.02

---
+++ script.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("wheel");
+const ctx = canvas.getcontext("2d");
+const spinbutton = document.getElementById("spinbutton");
+const result = document.getElementById("result");
